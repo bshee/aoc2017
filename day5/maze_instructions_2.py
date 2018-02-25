a42477547c64252e66a989fea39d26dffd839a09
Brian Shee <bri6294an@gmail.com>
2018-02-25 11:37:13 -0800

    Add solution for day 5, part 2

---
+++ day5/maze_instructions_2.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+THRESHOLD = 3
+
+filename = input('Input the filename: ')
+
+if not filename: filename = 'input'
+
+# The number of instructions that need to be followed to exit the maze.
+steps = 0
+program_counter = 0
+with open(filename) as f:
+  # Store the list of instructions on each line as a List.
+  instructions = [int(instruction) for instruction in f.readlines()]
+  num_instructions = len(instructions)
+
+  # Determine if the CPU has exited.
+  def exited_maze():
+    return program_counter < 0 or program_counter >= num_instructions
+
+  # Assume each list of instructions does contain a way to exit.
+  while True:
+    offset = instructions[program_counter]
+    current_change = -1 if offset >= THRESHOLD else 1
+    instructions[program_counter] += current_change
+    program_counter += offset
+    steps += 1
+    if exited_maze(): break
+
+print(steps)
